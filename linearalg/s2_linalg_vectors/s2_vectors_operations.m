580cf241229edcd71faabb74668ae3022b524fe5
XiaofangChu <xfang.chu@gmail.com>
2020-12-01 22:47:40 -0800

    update

--- linearalg/s2_linalg_vectors/s2_vectors_operations.m
+++ linearalg/s2_linalg_vectors/s2_vectors_operations.m
@@ -86,7 +86,80 @@ disp([res3 res4'])
 
 % Vector length
 
+v1 = [1 2 3 4 5 6];
 
+vl = sqrt(sum(v1.*v1));
+
+vl = norm(v1);
+
+% compute the angle (radians) between two vectors
+v1 = [ 2  4 -3 ];
+v2 = [ 0 -3 -3 ];
+
+ang = acos(dot(v1, v2)/(norm(v1)*norm(v2)));
+
+% draw them
+figure(4), clf
+plot3([0 v1(1)], [0 v1(2)], [0 v1(3)], 'b', 'linew', 2)
+hold on
+plot3([0 v2(1)],[0 v2(2)],[0 v2(3)],'r','linew',2)
+
+axmax = max([norm(v1) norm(v2)]);
+axis([-1 1 -1 1 -1 1]*axmax)
+grid on, rotate3d on, axis square
+title([ 'Angle between vectors: ' num2str(rad2deg(ang)) '^0' ])
+
+%% equivalence of algebraic and geometric dot product formulas
+
+% two vectors
+v1 = [ 2  4 -3 ];
+v2 = [ 0 -3 -3 ];
+
+
+% algebraic
+dp_a = sum( v1.*v2 );
+
+% geometric
+dp_g = norm(v1)*norm(v2)*cos(ang);
+
+% print dot product to command
+disp([ 'Algebra: ' num2str(dp_a) ', geometry: ' num2str(dp_g) ])
+
+% Vector Hadamard multiplication - element-wise
+
+w1 = [ 1 3 5 ];
+w2 = [ 3 4 2 ];
+
+w3 = w1 .* w2;
+
+% Vector outer product
+v1 = [1 2 3]';
+v2 = [-1 0]';
+
+% dot product;
+v1'*v2
+
+% outer product;
+v1*v2'
+
+op = zeros(length(v1), length(v2));
+for i = 1:length(v1)
+    for j = 1:length(v2)
+        op(i,j) = v1(i)*v2(j);
+    end
+end
+
+% Vector cross product
+v1  = [ -3  2 5 ];
+v2  = [  4 -3 0 ];
+
+v3a = cross(v1, v2);
+
+% manual method
+v3b = [ v1(2)*v2(3) - v1(3)*v2(2);
+        v1(3)*v2(1) - v1(1)*v2(3);
+        v1(1)*v2(2) - v1(2)*v2(1);
+    ]
 
 
 
