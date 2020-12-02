95e7fbff1a2d056d098ac3195c86ddaf79091b74
XiaofangChu <xfang.chu@gmail.com>
2020-12-01 23:49:54 -0800

    update

--- linearalg/s2_linalg_vectors/s2_vectors_operations.m
+++ linearalg/s2_linalg_vectors/s2_vectors_operations.m
@@ -161,6 +161,9 @@ v3b = [ v1(2)*v2(3) - v1(3)*v2(2);
         v1(1)*v2(2) - v1(2)*v2(1);
     ]
 
+% draw vectors and the plane they span
+figure(5), clf, hold on
+h = ezmesh( @(s,t)v1(1)*s+v2(1)*t , @(s,t)v1(2)*s+v2(2)*t , @(s,t)v1(3)*s+v2(3)*t , [-1 1 -1 1]*2);
 
 
 
